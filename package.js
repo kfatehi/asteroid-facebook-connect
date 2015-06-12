1f934207988529d9e41ca50a1b21065e362aa05b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-06-12 09:15:37 -0700

    bump to 0.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'keyvan:asteroid-facebook-connect',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'Meteor package providing support for Facebook Connect via Asteroid clients',
   // URL to the Git repository containing the source code for this package.
